ba91d86b44f0e033534c7b0995c2576a85f6d0cc
安小游 <471607346@qq.com>
2018-03-02 13:23:51 +0800

    add

--- resources/assets/js/components/common/TopMenu.vue
+++ resources/assets/js/components/common/TopMenu.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an menu component.  
+                        I'm an menu component.
                     </div>
                 </div>
             </div>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/',function (){
+    //Redis::set('name','yinchuanjiang');
+    return Redis::get('name');
 });
